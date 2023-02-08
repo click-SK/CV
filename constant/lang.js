9d851305b3784443d1427822125c14da7dd5f0f1
click-SK <sashakushnir4991@gmail.com>
2023-02-08 21:38:09 +0200

    lng class

---
+++ constant/lang.js
@@ -0,0 +1,147 @@
+const langArr = {
+    'title':{
+        "ua": 'КУШНІР ОЛЕКСАНДР',
+        "en": 'KUSHNIR OLEKSANDR'
+    },
+    'sub-title':{
+        "ua": 'Front End Developer, HTML coder, site designer',
+        "en": 'Front End Developer, HTML coder, site designer'
+    },
+    'contact':{
+        "ua": 'Контакти',
+        "en": 'Contacts'
+    },
+    'education':{
+        "ua": 'Навчання',
+        "en": 'Education'
+    },
+    'skills':{
+        "ua": 'Навички',
+        "en": 'Skills'
+    },
+    'experience':{
+        "ua": 'Досвід роботи',
+        "en": 'Experience'
+    },
+    'mob':{
+        "ua": 'Тел:',
+        "en": 'mob:'
+    },
+    'portfolio':{
+        "ua": 'Портфоліо:',
+        "en": 'Portfolio:'
+    },
+    'projects':{
+        "ua": 'Мої роботи:',
+        "en": 'My projects:'
+    },
+    'institution':{
+        "ua": 'Львівська філія Приватного вищого навчального закладу "Європейський університет"',
+        "en": 'Private higher educational institution "European University"'
+    },
+    'zabolotne':{
+        "ua": 'Zabolotne higher specialized school No. 31 named after D.K. Zabolotny',
+        "en": 'Заболотне вище профільне училище №31 ім. Д.К. Заболотного'
+    },
+    'inform':{
+        "ua": 'Information systems and technologies from 2022.',
+        "en": 'Інформаційні системи та технології з 2022 р.'
+    },
+    'management':{
+        "ua": 'Менеджментз 2016 по 2019 р.',
+        "en": 'Management from 2016 to 2019',
+    },
+    'secretary':{
+        "ua": 'Оператор комп`ютерного набору, секретар, з 2009 по 2012 р.',
+        "en": 'Computer typing operator, secretary, from 2009 to 2012',
+    },
+    'technologies':{
+        "ua": 'Використовую технології та методології:',
+        "en": 'I use technologies and methodologies:',
+    },
+    'bem':{
+        "ua": '•   БEМ',
+        "en": '•   BEM',
+    },
+    'svg':{
+        "ua": '•   Досвід роботи з SVG',
+        "en": '•   Experience with SVG',
+    },
+    'tools':{
+        "ua": 'Інструменти:',
+        "en": 'Tools:',
+    },
+    'lang':{
+        "ua": 'Мови:',
+        "en": 'Language:',
+    },
+    'english':{
+        "ua": '•   Англійська - pre-intermediate',
+        "en": '•   English - pre-intermediate',
+    },
+    'ukrain':{
+        "ua": '•   Українська - вільно',
+        "en": '•   Ukrainian - native language',
+    },
+    'ru':{
+        "ua": '•   Російська - вільно',
+        "en": '•   Russian - advenced',
+    },
+    'freelance':{
+        "ua": 'Фриланс проекти',
+        "en": 'Freelancer',
+    },
+    'freelanse-date':{
+        "ua": 'з 11.2021',
+        "en": 'from 11.2021',
+    },
+    'layout':{
+        "ua": 'HTML - верстка',
+        "en": 'HTML - layout',
+    },
+    'js-comp':{
+        "ua": 'JS - компоненти',
+        "en": 'JS - components',
+    },
+    'logo':{
+        "ua": 'Створення логотипів.',
+        "en": 'Creation of logos.',
+    },
+    'brand':{
+        "ua": 'Брендбуку.',
+        "en": 'Brandbook.',
+    },
+    'baners':{
+        "ua": 'Редагування фото.Створення банерів',
+        "en": 'Photo editing. Creating banners',
+    },
+    'coyot':{
+        "ua": 'Керівник відділу контенту',
+        "en": 'Head of the content department',
+    },
+    'coyot':{
+        "ua": 'Rерівник відділу контенту',
+        "en": 'Head of the content department',
+    },
+    'coyot-date':{
+        "ua": 'з 01.2023',
+        "en": 'from 01.2023',
+    },
+    'vilk':{
+        "ua": 'Контент-менеджер сайту',
+        "en": 'Site content manager',
+    },
+    'vilk-date':{
+        "ua": 'з 12.2021 по 10.2022 (10 місяців)',
+        "en": 'from 12.2021 to 10.2022 (10 months)',
+    },
+    'alz':{
+        "ua": 'Контент-менеджер сайту',
+        "en": 'Site content manager',
+    },
+    'alz-date':{
+        "ua": 'з 03.2019 по 11.2021 (2 роки 8 місяців)',
+        "en": 'from 03.2019 to 11.2021 (2 years 8 months)',
+    }
+
+}
